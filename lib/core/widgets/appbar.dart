11372bfba4f6afcc3320fbcfcec5dfdc6e30becd
zehraGltekin <zehragltekin12@gmail.com>
2025-09-01 23:43:10 +0300

    kvkksayfasi eklendi

--- lib/core/widgets/appbar.dart
+++ lib/core/widgets/appbar.dart
@@ -4,11 +4,19 @@ import 'package:sportzenzehra/core/theme/app_colors.dart';
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   final String title;
   final Widget? leading;
+  final PreferredSizeWidget? bottom;
+  final double? toolbarHeight;
 
-  const CustomAppBar({super.key, required this.title, this.leading});
+  const CustomAppBar({
+    super.key,
+    required this.title,
+    this.leading,
+    this.bottom,
+    this.toolbarHeight,
+  });
 
   @override
-  Size get preferredSize => Size.fromHeight(kToolbarHeight);
+  Size get preferredSize => Size.fromHeight((toolbarHeight ?? kToolbarHeight));
   @override
   Widget build(BuildContext context) {
     return AppBar(
@@ -22,6 +30,7 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
           fontWeight: FontWeight.w500,
         ),
       ),
+      bottom: bottom,
       leading: leading,
       elevation: 0,
       shape: Border(
--- lib/feature/settings/view/kvkk_view.dart
+++ lib/feature/settings/view/kvkk_view.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:sportzenzehra/core/theme/app_colors.dart';
+import 'package:sportzenzehra/core/widgets/appbar.dart';
+import 'package:sportzenzehra/feature/login/data/tab_content_model.dart';
+import 'package:sportzenzehra/feature/login/view/widgets/tab_content_card.dart';
 
 class KvkkView extends StatefulWidget {
   const KvkkView({super.key});
@@ -10,6 +15,80 @@ class KvkkView extends StatefulWidget {
 class _KvkkViewState extends State<KvkkView> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: CustomAppBar(
+          toolbarHeight: 100,
+          title: "Yasal Politikalar",
+          leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back_ios_new_rounded,
+              color: Theme.of(context).colorScheme.secondary,
+              size: 20,
+            ),
+            onPressed: () {
+              context.pop();
+            },
+          ),
+          bottom: TabBar(
+            dividerColor: AppColors.grey.withValues(alpha: 0.5),
+            labelColor: Theme.of(context).colorScheme.secondary,
+            unselectedLabelColor: AppColors.grey,
+            indicatorColor: Theme.of(context).colorScheme.secondary,
+            indicatorWeight: 3,
+            indicatorSize: TabBarIndicatorSize.tab,
+            tabs: const [
+              Tab(text: "KVKK"),
+              Tab(text: "Hizmet Şartları"),
+              Tab(text: "Gizlilik Politikası"),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            Column(
+              children: kvkkSections
+                  .map(
+                    (e) => Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 10,
+                        vertical: 10,
+                      ),
+                      child: TabContent(title: e.title, subtitle: e.subtitle),
+                    ),
+                  )
+                  .toList(),
+            ),
+            Column(
+              children: serviceSections
+                  .map(
+                    (e) => Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 10,
+                        vertical: 10,
+                      ),
+                      child: TabContent(title: e.title, subtitle: e.subtitle),
+                    ),
+                  )
+                  .toList(),
+            ),
+            Column(
+              children: securitySections
+                  .map(
+                    (e) => Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 10,
+                        vertical: 10,
+                      ),
+                      child: TabContent(title: e.title, subtitle: e.subtitle),
+                    ),
+                  )
+                  .toList(),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
