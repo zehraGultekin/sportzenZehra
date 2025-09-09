399fd13d689acd128e18d02ac230203368f88dc5
zehraGltekin <zehragltekin12@gmail.com>
2025-09-09 08:16:34 +0300

    .

--- lib/feature/home/data/mock/reservation_mock_data.dart
+++ lib/feature/home/data/mock/reservation_mock_data.dart
@@ -3,43 +3,43 @@ import 'package:sportzenzehra/feature/home/data/models/reservation2_model.dart';
 
 final List<ReservationModel> kort4 = [
   ReservationModel(
-    icon: Icon(Icons.wb_sunny_rounded, color: Colors.amber),
+    icon: Icon(Icons.wb_sunny, color: Colors.orange, size: 16),
     degree: "25.9C",
     clock: "12:00",
     state: "Aktif",
   ),
   ReservationModel(
-    icon: Icon(Icons.cloudy_snowing, color: Colors.amber),
+    icon: Icon(Icons.cloud, color: Colors.white, size: 16),
     degree: "26C",
-    clock: "123:00",
+    clock: "12:00",
     state: "Aktif",
   ),
   ReservationModel(
-    icon: Icon(Icons.wb_sunny_rounded, color: Colors.amber),
+    icon: Icon(Icons.wb_sunny_rounded, color: Colors.amber, size: 16),
     degree: "25.9C",
     clock: "14:00",
     state: "Aktif",
   ),
   ReservationModel(
-    icon: Icon(Icons.wb_sunny_rounded, color: Colors.amber),
+    icon: Icon(Icons.nightlight_rounded, color: Colors.indigo, size: 16),
     degree: "25.9C",
     clock: "15:00",
     state: "Aktif",
   ),
   ReservationModel(
-    icon: Icon(Icons.cloudy_snowing, color: Colors.amber),
+    icon: Icon(Icons.cloudy_snowing, color: Colors.white, size: 16),
     degree: "26C",
     clock: "123:00",
     state: "Aktif",
   ),
   ReservationModel(
-    icon: Icon(Icons.wb_sunny_rounded, color: Colors.amber),
+    icon: Icon(Icons.wb_sunny, color: Colors.orange, size: 16),
     degree: "25.9C",
     clock: "14:00",
     state: "Aktif",
   ),
   ReservationModel(
-    icon: Icon(Icons.wb_sunny_rounded, color: Colors.amber),
+    icon: Icon(Icons.nightlight, color: Colors.indigo, size: 16),
     degree: "25.9C",
     clock: "15:00",
     state: "Aktif",
